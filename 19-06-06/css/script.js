016ba54cb0713923efee4d9a98747d5ec2c13428
Karolis <karolisliesys@gmail.com>
2019-06-06 16:11:38 +0300

    fixed <= to <

--- 19-06-06/css/script.js
+++ 19-06-06/css/script.js
@@ -1,10 +1,10 @@
 function clickMe(){
-    let x = Math.floor(Math.random() * 15);
+    let x = Math.floor(Math.random() * 14);
 
     let z = document.getElementById("output");
 
-    // document.getElementById("test").innerHTML = x;
-    if(x <= 1){
+    document.getElementById("test").innerHTML = x;
+    if(x < 1){
         z.innerHTML = ("You got 3 tickets");
     }else if(x <= 2){
         z.innerHTML = ("You got 2 tickets");
