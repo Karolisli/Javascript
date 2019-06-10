ec71083fccc9b4ec1210fa411edfe74b8110ee0d
Karolis <karolisliesys@gmail.com>
2019-06-10 13:01:06 +0300

    I

--- 19-06-10/css/script.js
+++ 19-06-10/css/script.js
@@ -1,11 +1,25 @@
 function solution(X) {
     var a = " ";
+    // top
+    for(let x = 0; x < X/4 ; x++){
+        for(let y = 0; y < X*2; y++){
+            a += "*"
+        }
+        a += "<br>"
+    }
+    // middle
     for (let i = 0; i < X; i++) {
+        // invisible
+        for(let d = 0; d < (X*6)/4; d++){
+            a+= "&nbsp";
+        }
+        // visible
         for(let s = 0; s < X/2; s++) {
             a += "*";
         }
         a += "<br>";
     }
+    // bottom
     for(let x = 0; x < X/4 ; x++){
         for(let y = 0; y < X*2; y++){
             a += "*"
@@ -15,4 +29,4 @@ function solution(X) {
     return a;
 }
 
-document.write(solution(9));
+document.write(solution(12));
